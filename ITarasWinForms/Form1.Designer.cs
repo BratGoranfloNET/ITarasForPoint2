c57da16d8fabd240458ff3344bac35e7b800ac0e
iTaras <bratgoranflo@gmail.com>
2020-08-10 14:05:45 +0700

    first commit

---
+++ ITarasWinForms/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿using Karambolo.PO;
+using System.IO;
+
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(22, 12);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(367, 408);
+            this.treeView1.TabIndex = 0;
+            this.treeView1.DoubleClick += new System.EventHandler(this.treeView1_DoubleClick);
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(395, 12);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(286, 408);
+            this.listView1.TabIndex = 2;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.treeView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.ListView listView1;
+    }
+}
+
---
+++ ITarasWinForms/Form1.cs
@@ -0,0 +1,98 @@
+﻿using Karambolo.PO;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public List<Node> nodesList = new List<Node>();
+        public Form1()
+        {
+            InitializeComponent();
+
+            var parser = new POParser();
+            POParseResult result;
+            using (var reader = new StreamReader(@"C:\LOGTEST\example.po"))
+            result = parser.Parse(reader);
+            if (result.Success)
+            {
+                POCatalog catalog = result.Catalog;
+                
+                int index = 0;               
+                foreach (IPOEntry item in catalog)
+                {
+                    var context = item.Key.ContextId;
+                    string[] nodes = context.Split('.');
+
+                    Node nodeListitem = new Node()
+                    {
+                        Levels = nodes.Length,
+                        Nodes = nodes,   
+                        Names = context,
+                        Id = item.Key.Id                        
+                    };
+
+                    int treeLevelCount = 1;                     
+                    foreach (string node in nodes)
+                    {
+                        List<Node> fullNodNameTemp = nodesList.Where(x => x.Names == context).ToList();
+                        if (fullNodNameTemp.Count == 0)
+                        {
+                            List<Node> nodeNameTemp = nodesList.Where(x => x.Nodes.Contains(node)).ToList();
+
+                            if (nodeNameTemp.Count == 0)
+                            {
+                                switch (treeLevelCount)
+                                {
+                                    case 1:
+                                        treeView1.Nodes.Add(node, node);                                        
+                                        break;
+                                    case 2:
+                                        treeView1.Nodes[index].Nodes.Add(node, node);                                        
+                                        break;
+                                    case 3:
+                                        treeView1.Nodes[index].Nodes[0].Nodes.Add(node, node);
+                                        index++;                                                                            
+                                        break;
+                                    default:                                        
+                                        break;
+                                }                                
+                            }                            
+                        }                       
+                        treeLevelCount++;                        
+                    }
+                    nodesList.Add(nodeListitem);
+                }                
+            }
+            else
+            {
+                IDiagnostics diagnostics = result.Diagnostics;                
+            }
+        }
+
+
+        private void treeView1_DoubleClick(object sender, EventArgs e)
+        {            
+            List<Node> selectedNodes = nodesList.Where(x => x.Nodes.Contains(treeView1.SelectedNode.Name)).ToList();
+            listView1.Clear();
+            foreach (Node item in selectedNodes)
+            {
+                listView1.Items.Add(item.Id);
+            }       
+        }
+    }
+
+    public class Node
+    {
+        public string Id { get; set; }
+        public int Levels { get; set; }
+        public string Names { get; set; }
+        public string[] Nodes { get; set; }     
+        
+    }
+}
